a30d9cdd8305485b1602c106b68a5154c0213304
Josh Pollock <jopo@mit.edu>
2022-12-19 20:00:47 -0800

    nope have to use use-gesture for svgs

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,23 @@ import { Droppable } from "./components/Droppable";
 
 function App() {
   const [isDropped, setIsDropped] = useState(false);
+  const [circlePosition, setCirclePosition] = useState({
+    x: 20,
+    y: 20,
+  });
   const draggableMarkup = <Draggable>Drag me</Draggable>;
 
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
     if (active.id === "draggable" && over?.id === "droppable") {
       setIsDropped(true);
+    }
+
+    if (active.id === "circle-0") {
+      setCirclePosition(({ x, y }) => ({
+        x: x + event.delta.x,
+        y: y + event.delta.y,
+      }));
     }
   };
 
@@ -21,9 +32,14 @@ function App() {
         {!isDropped && draggableMarkup}
         <Droppable>{isDropped ? draggableMarkup : "Drag here"}</Droppable>
       </DndContext>
-      <svg width="100%" height="100%" viewBox="0 0 100 100">
-        <Circle x={20} y={20} radius={5} fill={"cornflowerblue"} />
-        <Circle x={40} y={30} radius={2} fill={"firebrick"} />
+      <svg width="1000" height="1000">
+        <Circle
+          id="0"
+          {...circlePosition}
+          radius={50}
+          fill={"cornflowerblue"}
+        />
+        <Circle id="1" x={40} y={30} radius={20} fill={"firebrick"} />
       </svg>
     </>
   );
--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,10 +1,37 @@
+import { useGesture } from "@use-gesture/react";
+import { useState } from "react";
+
 export type CircleProps = {
   x: number;
   y: number;
   radius: number;
   fill: string;
+  id: string;
 };
 
-export const Circle = ({ x, y, radius, fill }: CircleProps) => (
-  <circle cx={x} cy={y} r={radius} fill={fill} />
-);
+export const Circle = ({
+  id,
+  x: xInit,
+  y: yInit,
+  radius,
+  fill,
+}: CircleProps) => {
+  const [{ x, y }, setTranslate] = useState({ x: 0, y: 0 });
+
+  const bind = useGesture({
+    onDrag: ({ offset: [x, y] }) => {
+      setTranslate({ x, y });
+    },
+  });
+
+  return (
+    <circle
+      cx={xInit}
+      cy={yInit}
+      r={radius}
+      fill={fill}
+      transform={`translate(${x}, ${y})`}
+      {...bind()}
+    />
+  );
+};
