7b8e71f38a861db89a12a2bb3c574b59012a39f6
Josh Pollock <jopo@mit.edu>
2022-12-23 12:32:53 -0800

    bidirectional editing

--- src/components/RenderContext.tsx
+++ src/components/RenderContext.tsx
@@ -3,11 +3,44 @@ import { CircleContext } from "../Context";
 
 export const RenderContext = () => {
   const circleContext = React.useContext(CircleContext);
+
+  // create a handleChange function that takes an id and field name and returns a function that
+  // takes an event and updates the context
+  const handleChange = (id: string, field: string) => (e: any) => {
+    circleContext.setCircleData(id, {
+      ...circleContext.circleData[id],
+      [field]: e.target.value,
+    });
+  };
+
   return (
     <>
       {Object.entries(circleContext.circleData).map(([id, circle]) => (
         <div key={id}>
           {id}: {JSON.stringify(circle)}
+          <br />
+          x: <input value={circle.x} onChange={handleChange(id, "x")} />
+          <br />
+          y: <input value={circle.y} onChange={handleChange(id, "y")} />
+          <br />
+          translateX:{" "}
+          <input
+            value={circle.translateX}
+            onChange={handleChange(id, "translateX")}
+          />
+          <br />
+          translateY:{" "}
+          <input
+            value={circle.translateY}
+            onChange={handleChange(id, "translateY")}
+          />
+          <br />
+          radius:{" "}
+          <input value={circle.radius} onChange={handleChange(id, "radius")} />
+          <br />
+          fill:{" "}
+          <input value={circle.fill} onChange={handleChange(id, "fill")} />
+          <br />
         </div>
       ))}
     </>
