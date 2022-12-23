dfa29ec87be160482fa9b2f691aac70e8277e2dc
Josh Pollock <jopo@mit.edu>
2022-12-23 13:17:25 -0800

    some notes and removing stringify

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 Bidirectional editor for Bluefish components. How hard could it really be??
 
 This repo was initialized using architectural notes from ChatGPT.
+
+# Random Notes
+
+I can now manipulate two circles individually.
+
+The next thing to try out is align and/or spacing components via a combination of direct
+manipulation (typically drop-down menu or similar) and editing code directly.
+One question is going to be how to resolve edits once the align/space is persistent. We want to
+conservatively extend GUIs so you should probably be able to select individual elements and/or the
+group and then do something with it. But you should also be able to make larger scale edits using
+code or some special propagation mode or something.
--- src/components/RenderContext.tsx
+++ src/components/RenderContext.tsx
@@ -16,32 +16,37 @@ export const RenderContext = () => {
   return (
     <>
       {Object.entries(circleContext.circleData).map(([id, circle]) => (
-        <div key={id}>
-          {id}: {JSON.stringify(circle)}
+        <>
+          <div key={id}>
+            {/* {id}: {JSON.stringify(circle)}
+          <br /> */}
+            x: <input value={circle.x} onChange={handleChange(id, "x")} />
+            <br />
+            y: <input value={circle.y} onChange={handleChange(id, "y")} />
+            <br />
+            translateX:{" "}
+            <input
+              value={circle.translateX}
+              onChange={handleChange(id, "translateX")}
+            />
+            <br />
+            translateY:{" "}
+            <input
+              value={circle.translateY}
+              onChange={handleChange(id, "translateY")}
+            />
+            <br />
+            radius:{" "}
+            <input
+              value={circle.radius}
+              onChange={handleChange(id, "radius")}
+            />
+            <br />
+            fill:{" "}
+            <input value={circle.fill} onChange={handleChange(id, "fill")} />
+          </div>
           <br />
-          x: <input value={circle.x} onChange={handleChange(id, "x")} />
-          <br />
-          y: <input value={circle.y} onChange={handleChange(id, "y")} />
-          <br />
-          translateX:{" "}
-          <input
-            value={circle.translateX}
-            onChange={handleChange(id, "translateX")}
-          />
-          <br />
-          translateY:{" "}
-          <input
-            value={circle.translateY}
-            onChange={handleChange(id, "translateY")}
-          />
-          <br />
-          radius:{" "}
-          <input value={circle.radius} onChange={handleChange(id, "radius")} />
-          <br />
-          fill:{" "}
-          <input value={circle.fill} onChange={handleChange(id, "fill")} />
-          <br />
-        </div>
+        </>
       ))}
     </>
   );
